a9d09953e6742462ab460eca169760bf0673b9cb
eriktalvi <etalvi@alaska.edu>
2014-03-22 04:13:43 +0000

    this should work ?!??!

---
+++ BattleShip/ShipStrucs.h
@@ -0,0 +1,42 @@
+//
+//  ShipStrucs.h
+//  BattleShip
+//
+//  Created by Prodigy on 3/21/14.
+//  Copyright (c) 2014 Prodigy. All rights reserved.
+//
+
+#ifndef BattleShip_ShipStrucs_h
+#define BattleShip_ShipStrucs_h
+
+struct aircraftCarrier
+{
+    int hole1, hole2, hole3, hole4, hole5 = NULL;
+};
+
+struct battleship
+{
+    int hole1, hole2, hole3, hole4 = NULL;
+};
+
+struct submarine
+{
+    int hole1, hole2, hole3 = NULL;
+};
+
+struct destroyer
+{
+    int hole1, hole2, hole3 = NULL;
+};
+
+struct patrolBoat
+{
+    int hole1, hole2 = NULL;
+};
+
+int attack(int coordinates)
+{
+    if (aircraftCarrier
+}
+
+#endif
---
+++ BattleShip/board.h
@@ -0,0 +1,19 @@
+//
+//  board.h
+//  BattleShip
+//
+//  Created by Prodigy on 3/21/14.
+//  Copyright (c) 2014 Prodigy. All rights reserved.
+//
+
+#ifndef BattleShip_board_h
+#define BattleShip_board_h
+
+struct board
+{
+    boardOne[10][10] = 0;
+    boardTwo[10][10] = 0;
+};
+
+
+#endif
